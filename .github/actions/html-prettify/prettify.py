d66e1cd134a8c88b65c3540428e670a05f5d901e
Giles Antonio Radford <garadford@transcomashipping.com>
2020-10-25 23:48:29 +0100

    🐛 Ponemos la codificación utf8

--- .github/actions/html-prettify/prettify.py
+++ .github/actions/html-prettify/prettify.py
@@ -2,5 +2,5 @@ import sys
 from bs4 import BeautifulSoup
 with open(sys.argv[1], 'rb') as f:
     b = BeautifulSoup(f, features="html.parser")
-with open(sys.argv[1], 'w') as f:
+with open(sys.argv[1], 'w', encoding="utf8") as f:
     f.write(b.prettify())
