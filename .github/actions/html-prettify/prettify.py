da131bf371ac750d494b5b445d2c4ac3b8d43eff
Giles Antonio Radford <garadford@transcomashipping.com>
2020-10-25 23:45:22 +0100

    💚 Pasamos la accion a un script separado

---
+++ .github/actions/html-prettify/prettify.py
@@ -0,0 +1,6 @@
+import sys
+from bs4 import BeautifulSoup
+with open(sys.argv[1], 'rb') as f:
+    b = BeautifulSoup(f, features="html.parser")
+with open(sys.argv[1], 'w') as f:
+    f.write(b.prettify())
