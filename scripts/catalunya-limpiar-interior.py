7a8e34353dc8a8652a9c8b55156469f37692c9a2
Giles Antonio Radford <garadford@transcomashipping.com>
2020-10-26 21:34:35 +0100

    🌱 [Catalunya] Limpiamos las páginas de interior

---
+++ scripts/catalunya-limpiar-interior.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from bs4 import BeautifulSoup
+import re
+
+
+SOURCE_FOLDER = Path("documentos/España/Catalunya/interior") 
+files = [f for f in SOURCE_FOLDER.iterdir() if f.suffix == ".html"] 
+
+for f in files:
+    b = BeautifulSoup(f.read_bytes(), features="html.parser")
+    tag = b.find(property="og:url")
+    tag["content"] = "#reemplazado"
+    s = b.find(string=re.compile("GEOATX"))
+    s.replace_with("#reemplazado")
+    f.write_bytes(b.encode("utf8", formatter=None))
