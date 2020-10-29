100a3426afb9fdbac1e6c75a542b60c88e9c0a8c
Giles Antonio Radford <garadford@transcomashipping.com>
2020-10-29 15:55:54 +0100

    ♻️ Refactorizamos y arreglamos descarga Normativa

--- scripts/extraer-normativa-europea-y-autonomica-boe.py
+++ scripts/extraer-normativa-europea-y-autonomica-boe.py
@@ -4,32 +4,37 @@ import tempfile
 from pathlib import Path
 import os
 
-EU_URL = "https://www.boe.es/biblioteca_juridica/codigos/abrir_epub.php?fich=355_COVID-19_Derecho_Europeo_y_Estatal__.epub"
-EU_OUTPUT = Path("documentos/nacional/boe/codigos/normativa-ue.xhtml")
 
-r = requests.get(EU_URL)
+def extract_epub_page(url, href, outfile):
 
-(f, fname) = tempfile.mkstemp("epub")
-f = os.fdopen(f, "wb")
-f.write(r.content)
-f.close()
+    print(f"Extrayendo {url} a {outfile}...")
+    r = requests.get(url)
+    (f, fname) = tempfile.mkstemp("epub")
+    f = os.fdopen(f, "wb")
+    f.write(r.content)
+    f.close()
 
-book = epub.read_epub(fname)
-doc = book.get_item_with_href("nota-autor.xhtml")
+    book = epub.read_epub(fname)
+    doc = book.get_item_with_href(href)
 
-EU_OUTPUT.write_bytes(doc.get_content())
+    outpath = Path(outfile)
+    outpath.write_bytes(doc.get_content())
 
-AUTONOMY_URL = "https://www.boe.es/biblioteca_juridica/codigos/abrir_epub.php?fich=396_COVID-19_Derecho_Autonomico.epub"
-AUTONOMY_OUTPUT = Path("documentos/nacional/boe/codigos/normativa-autonómica.xhtml")
-
-f = os.fdopen(f, "wb")
-f.write(r.content)
-f.close()
-
-book = epub.read_epub(fname)
-doc = book.get_item_with_href("nota-autor.xhtml")
-
-AUTONOMY_OUTPUT.write_bytes(doc.get_content())
+    os.remove(fname)
 
 
-os.remove(fname)
+# Primero la normativa UE:
+extract_epub_page(
+    "https://www.boe.es/biblioteca_juridica/codigos/abrir_epub.php?" +
+    "fich=355_COVID-19_Derecho_Europeo_y_Estatal__.epub",
+    "nota-autor.xhtml",
+    "documentos/nacional/boe/codigos/normativa-ue.xhtml"
+)
+
+# Luego la normativa aotnómica
+extract_epub_page(
+    "https://www.boe.es/biblioteca_juridica/codigos/abrir_epub.php?" +
+    "?fich=396_COVID-19_Derecho_Autonomico.epub",
+    "nota-autor.xhtml",
+    "documentos/nacional/boe/codigos/normativa-autonómica.xhtml"
+)
